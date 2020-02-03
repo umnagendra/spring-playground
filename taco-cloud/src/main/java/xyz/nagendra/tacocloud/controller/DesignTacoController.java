edd9e46e855cfc6c52a7a5a1b78f3cfff1ff5ab2
Nagendra Mahesh <namahesh@cisco.com>
2020-02-03 16:53:48 +0530

    Design controller and web form

---
+++ taco-cloud/src/main/java/xyz/nagendra/tacocloud/controller/DesignTacoController.java
@@ -0,0 +1,62 @@
+package xyz.nagendra.tacocloud.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import xyz.nagendra.tacocloud.Ingredient;
+import xyz.nagendra.tacocloud.Ingredient.Type;
+import xyz.nagendra.tacocloud.Taco;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import static xyz.nagendra.tacocloud.Ingredient.Type.*;
+
+@Controller
+@RequestMapping("/design")
+public class DesignTacoController {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(DesignTacoController.class);
+
+    @GetMapping
+    public String showDesignForm(Model model) {
+        List<Ingredient> ingredients = Arrays.asList(
+                new Ingredient("FLTO", "Flour Tortilla", WRAP),
+                new Ingredient("COTO", "Corn Tortilla", WRAP),
+                new Ingredient("GRBF", "Ground Beef", PROTEIN),
+                new Ingredient("CARN", "Carnitas", PROTEIN),
+                new Ingredient("TMTO", "Diced Tomatoes", VEGGIES),
+                new Ingredient("LETC", "Lettuce", VEGGIES),
+                new Ingredient("CHED", "Cheddar", CHEESE),
+                new Ingredient("JACK", "Monterrey Jack", CHEESE),
+                new Ingredient("SLSA", "Salsa", SAUCE),
+                new Ingredient("SRCR", "Sour Cream", SAUCE)
+        );
+        Type[] types = Ingredient.Type.values();
+        for (Type type : types) {
+            model.addAttribute(type.toString().toLowerCase(),
+                    filterByType(ingredients, type));
+        }
+        model.addAttribute("design", new Taco());
+        return "design";
+    }
+
+    @PostMapping
+    public String processDesign(Taco taco) {
+        // TODO save the taco designs
+        LOGGER.info("Received taco design for saving: {}", taco);
+        return "/orders/current";
+    }
+
+    private List<Ingredient> filterByType(List<Ingredient> ingredients, Type type) {
+        return ingredients
+                .stream()
+                .filter(ingredient -> ingredient.getType() == type)
+                .collect(Collectors.toList());
+    }
+}
