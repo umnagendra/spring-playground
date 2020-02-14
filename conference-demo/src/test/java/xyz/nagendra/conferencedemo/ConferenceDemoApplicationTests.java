6ce00b9369f59274b7f1f8e1747b2adcda3de10a
Nagendra Mahesh <namahesh@cisco.com>
2020-02-14 09:05:36 +0530

    Pluralsight - conference demo app - initial structure

---
+++ conference-demo/src/test/java/xyz/nagendra/conferencedemo/ConferenceDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package xyz.nagendra.conferencedemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConferenceDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
