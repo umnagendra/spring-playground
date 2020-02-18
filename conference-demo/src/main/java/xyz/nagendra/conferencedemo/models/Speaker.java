636b385d18d1f4b0265892c83f48dac7ed9b0202
Nagendra Mahesh <namahesh@cisco.com>
2020-02-18 12:13:54 +0530

    misc.

--- conference-demo/src/main/java/xyz/nagendra/conferencedemo/models/Speaker.java
+++ conference-demo/src/main/java/xyz/nagendra/conferencedemo/models/Speaker.java
@@ -1,7 +1,6 @@
 package xyz.nagendra.conferencedemo.models;
 
 import com.fasterxml.jackson.annotation.JsonBackReference;
-import com.fasterxml.jackson.annotation.JsonManagedReference;
 import org.hibernate.annotations.Type;
 
 import javax.persistence.Column;
